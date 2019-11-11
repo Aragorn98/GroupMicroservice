271666cb17c0a5ba779960c6f23fa9bccacf4442
Argyn <argynaitkazy@gmail.com>
2019-11-11 14:05:31 +0600

    secure microservice for rest clients

--- src/main/java/com/example/groupCRUD/web/GroupController.java
+++ src/main/java/com/example/groupCRUD/web/GroupController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 //tag::baseClass[]
 
 @RestController
-@RequestMapping("/group")
+//@RequestMapping("/group")
 public class GroupController {
 
     private GroupRepository groupRepository;
